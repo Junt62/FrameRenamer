85c85ed99332bd8989a58af6036f87e2b4c13602
junt62 <junt62@qq.com>
2024-09-13 11:18:27 +0800

    updated.

---
+++ FrameRenamer.py
@@ -0,0 +1,141 @@
+from datetime import datetime
+import os
+import shutil
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+from FrameRenamer_ui import Ui_Form
+
+
+previewText = "重命名预览.txt"
+backupSuffix = "_备份"
+
+
+class MainForm(QtWidgets.QMainWindow, Ui_Form):
+    def __init__(self, parent=None):
+        super(MainForm, self).__init__(parent)
+        self.setupUi(self)
+
+        self.setAcceptDrops(True)
+
+        self.pushButton.pressed.connect(self.pressedPushButton1)
+        self.pushButton_2.pressed.connect(self.pressedPushButton2)
+        self.pushButton_3.pressed.connect(self.pressedPushButton3)
+        self.checkBox.stateChanged.connect(self.checkBoxChanged)
+
+        self.toolPackage = ToolPackage(self)
+
+        self.toolPackage.tint("请直接拖入含有图片的文件夹")
+        self.toolPackage.tint("软件会自动执行备份，支持png，jpg，bmp")
+        self.toolPackage.tint("当点击执行重命名时，会将文件夹内图片替换为")
+        self.toolPackage.tint("文件夹名+000的递增数字")
+
+    def dragEnterEvent(self, event: QtGui.QDragMoveEvent):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+
+    def dropEvent(self, event: QtGui.QDropEvent):
+        urls = event.mimeData().urls()
+        if urls:
+            self.path = urls[0].toLocalFile()
+            self.folder = os.path.basename(self.path)
+
+            self.lineEdit.setText(self.path)
+            self.toolPackage.tint("已拖入文件夹：" + self.folder)
+
+            self.updateLineEdit2()
+
+            imagesWithIndex = []
+            for i, k in enumerate(self.images):
+                num = f"{i:03}"
+                dot = k.rfind(".")
+                new = self.folder + num + k[dot:]
+                imagesWithIndex.append(new)
+            self.lineEdit_3.setText(f"{imagesWithIndex}")
+
+            if self.checkBox.isChecked():
+                self.pressedPushButton1()
+
+    def pressedPushButton1(self):
+        if self.lineEdit.text().strip():
+            images = self.toolPackage.findImages(self.path)
+            for i, k in enumerate(images):
+                num = f"{i:03}"
+                dot = k.rfind(".")
+                old = os.path.join(self.path, images[i])
+                old = old.replace("\\", "/")
+                new = os.path.join(self.path, self.folder + num + k[dot:])
+                new = new.replace("\\", "/")
+                os.rename(old, new)
+
+            self.toolPackage.tint("执行重命名完成！")
+            self.updateLineEdit2()
+            # print(old + " -> " + new)
+
+    def pressedPushButton2(self):
+        # self.toolPackage.tint("查看重命名预览")
+        path = os.path.join(os.path.dirname(__file__), previewText)
+        if os.path.exists(path):
+            os.startfile(path)
+
+    def pressedPushButton3(self):
+        # self.toolPackage.tint("打开备份文件夹")
+        os.startfile(os.path.dirname(__file__))
+
+    def checkBoxChanged(self, state):
+        # if state:
+        #     self.toolPackage.tint("启用自动执行重命名，拖入文件夹执行")
+        # else:
+        #     self.toolPackage.tint("禁用自动执行")
+        pass
+
+    def updateLineEdit2(self):
+        self.images = self.toolPackage.findImages(self.path)
+        self.lineEdit_2.setText(f"{self.images}")
+        self.toolPackage.generatePreviewText(self.folder, self.images)
+        self.toolPackage.generateBackupFolder(self.path, self.folder)
+
+
+class ToolPackage:
+    def __init__(self, MainForm):
+        self.MainForm = MainForm
+
+    def tint(self, text):
+        form = self.MainForm.textEdit
+        time = datetime.now().strftime("%H:%M:%S")
+        form.insertPlainText(f"【{time}】{text}\n")
+        form.moveCursor(QtGui.QTextCursor.End)
+
+    def findImages(self, path):
+        extensions = (".png", ".jpg", ".bmp")
+        images = []
+        for image in os.listdir(path):
+            if image.lower().endswith(extensions):
+                images.append(image)
+        return images
+
+    def generatePreviewText(self, folder, images):
+        path = os.path.join(os.path.dirname(__file__), previewText)
+        with open(path, "w", encoding="utf-8") as file:
+            for i, k in enumerate(images):
+                num = f"{i:03}"
+                dot = k.rfind(".")
+                new = folder + num + k[dot:]
+                file.write(k + " -> " + new + "\n")
+
+    def generateBackupFolder(self, path, folder):
+        target = os.path.join(os.path.dirname(__file__), folder + backupSuffix)
+
+        if os.path.exists(target):
+            shutil.rmtree(target)
+
+        shutil.copytree(path, target)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    widget = MainForm()
+    widget.setWindowTitle("贪玩-序列帧重命名工具v1.0.0@zijun")
+    widget.show()
+    app.exec()
---
+++ FrameRenamer_ui.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'FrameRenamer.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QLabel, QLineEdit,
+    QPushButton, QSizePolicy, QTextEdit, QWidget)
+import FrameRenamer_rc
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(400, 300)
+        Form.setMinimumSize(QSize(400, 300))
+        Form.setMaximumSize(QSize(400, 300))
+        icon = QIcon()
+        icon.addFile(u":/icon/icon.ico", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        Form.setWindowIcon(icon)
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 160, 71, 23))
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 10, 381, 16))
+        self.lineEdit = QLineEdit(Form)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(10, 30, 381, 21))
+        self.lineEdit.setReadOnly(True)
+        self.pushButton_2 = QPushButton(Form)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(90, 160, 61, 23))
+        self.label_2 = QLabel(Form)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 60, 381, 16))
+        self.lineEdit_2 = QLineEdit(Form)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setGeometry(QRect(10, 80, 381, 21))
+        self.lineEdit_2.setReadOnly(True)
+        self.label_3 = QLabel(Form)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(10, 110, 381, 16))
+        self.lineEdit_3 = QLineEdit(Form)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.lineEdit_3.setGeometry(QRect(10, 130, 381, 21))
+        self.lineEdit_3.setReadOnly(True)
+        self.label_4 = QLabel(Form)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(10, 190, 381, 16))
+        self.checkBox = QCheckBox(Form)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setGeometry(QRect(270, 162, 111, 19))
+        self.textEdit = QTextEdit(Form)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(10, 210, 381, 81))
+        self.textEdit.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        self.textEdit.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.textEdit.setReadOnly(True)
+        self.pushButton_3 = QPushButton(Form)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(160, 160, 91, 23))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"\u8d2a\u73a9-\u5e8f\u5217\u5e27\u6279\u91cf\u91cd\u547d\u540d\u5de5\u5177", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"\u6267\u884c\u91cd\u547d\u540d", None))
+        self.label.setText(QCoreApplication.translate("Form", u"\u5f53\u524d\u6587\u4ef6\u5939\u5730\u5740\uff1a", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Form", u"\u67e5\u770b\u9884\u89c8", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"\u5f53\u524d\u6587\u4ef6\u540d\uff1a", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"\u76ee\u6807\u6587\u4ef6\u540d\uff1a", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"\u63d0\u793a\u4fe1\u606f\uff1a", None))
+        self.checkBox.setText(QCoreApplication.translate("Form", u"\u81ea\u52a8\u6267\u884c\u91cd\u547d\u540d", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Form", u"\u6253\u5f00\u5907\u4efd\u6587\u4ef6\u5939", None))
+    # retranslateUi
+
